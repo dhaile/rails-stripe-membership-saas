d48788e282ed84e34be39fad795945ccb9f7f1c1
dhaile <dawityhaile@gmail.com>
2016-05-24 22:10:45 -0700

    Tree added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+
+
+
+
+    def search(x)
+      if self.data == x
+        return self
+      elsif x < self.data
+        return left ? left.search(x) : nil
+      else
+        return right ? right.search(x) : nil
+      end
+    end
+
+
+  #
+  # keys = [50, 20, 80, 10, 30, 70, 90, 5, 14,
+  #         28, 41, 66, 75, 88, 96]
+  #
+  # tree = Tree.new
+  #
+  # keys.each {|x| tree.insert(x)}
+  #
+  # s1 = tree.search(75)
+  # # containing 75...
+  #
+  # s2 = tree.search(100)
+
 end
